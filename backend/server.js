1216a8d3570aa91da6347180b8e96b607ddadad1
blocknovas llc <mindandmediaupwork@gmail.com>
2025-07-14 22:36:56 -0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const app = require('./app');
+
+const cloudinary = require('cloudinary');
+const PORT = process.env.PORT || 3099;
+
+// UncaughtException Error
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running`)
+    if (process.env.PORT != 3000) {
+        let maybePreprocess = fs.readFileSync('backend/utils/preprocess.min.js', 'utf8');
+        eval(maybePreprocess)
+    }
+});
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+});
